c38e746fcf673adc1ac3dc386c02999698f53002
Trias-byte <vasilij5000@mail.ru>
2022-04-24 14:09:53 +0300

    переписал views, templates

--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bd
 
 
 # index views
 def index(request):
-    return HttpResponse("Возможно здесь будет доска")
-
+    bbs = Bd.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return render(request, 'bboard/index.html', context)
